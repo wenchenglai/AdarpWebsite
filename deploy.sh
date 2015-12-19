e4b7876dff1cd68c48a0a28e1f42e7317e3b4e34
wenchenglai <wenchenglai@gmail.com>
2015-12-19 11:31:38 -0500

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+
+cp -R ../AdarpWebsite /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite
+appcfg.py -A seventh-dynamo-673 update /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite
