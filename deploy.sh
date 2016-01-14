093bbe5dd06114dc19c59ad79147db820d2cfad5
wenchenglai <wenchenglai@gmail.com>
2016-01-14 15:10:22 -0500

    more changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+rm -df /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite 
+mkdir /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite
+cp -v index.html /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite
+cp -vR css /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite
+cp -vR fonts /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite
+cp -vR img /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite
+cp -vR js /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite/AdarpWebsite
 
-cp -R ../AdarpWebsite /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite
 appcfg.py -A seventh-dynamo-673 update /Users/wen/Documents/"Google App Engine Projects"/AdarpWebsite
